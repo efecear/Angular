725ae152e7b0533466b626bcf1e56fef144d0dfe
Federico <github@efece.ar>
2022-02-28 13:51:22 -0300

    lista de personas anadidas a personas.component.ts

--- mi-primera-app-Angular-Github/src/app/componentes/personas/personas.component.ts
+++ mi-primera-app-Angular-Github/src/app/componentes/personas/personas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/models/persona';
 
 @Component({
   selector: 'app-personas',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent implements OnInit {
-
+// Atributos
+  title =  "Esta es nuestra lista de personas!";
+  personas: Persona[] = []
   constructor() { }
 
   ngOnInit(): void {
+    let persona1 = new Persona("Hernan", "Borre", 28)
+    this.personas.push(persona1)
+    this.personas.push(new Persona("Alejandro", "Fantino", 55))
+    this.personas.push(new Persona("Nicky", "Nicole", 22))
+    this.personas.push(new Persona("Federico", "Camarota"))
   }
 
 }
